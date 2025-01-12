0017f97bf083d73cf864ce5a3a1318d8b0e508eb
SumitBando-Suraksha <sumit.bando@surakshanet.com>
2025-01-12 18:00:44 +0530

    Inline CSS/JS uwing vite-plugin-singlefile

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
+// https://vite.dev/config/
+
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    viteSingleFile()
+  ],
+  build: {
+    cssCodeSplit: false,
+    cssMinify: true,
+    assetsInlineLimit: 100000000,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
