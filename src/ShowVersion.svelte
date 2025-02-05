29f45d7d5dd94d3a30534ec05d05a736d2255470
SumitBando <sumit@hotmail.com>
2025-02-05 16:56:52 +0530

    Remove version.json from root, should be in /public

--- src/ShowVersion.svelte
+++ src/ShowVersion.svelte
@@ -3,7 +3,7 @@
 		try {
 			const res = await fetch('/version.json')
 			const data = await res.json()
-			console.log(data)
+			// console.log(data)
 			return data
 		} catch (err) {
 			console.error('Error loading version info:', err)
@@ -12,5 +12,5 @@
 </script>
 
 {#await versionPromise then version}
-	<div class="mt-4 text-slate-400 text-center">Build date: {version?.buildDate.split('T')[0]} githash: {version?.commitHash.slice(0,7)}</div>
+	<div class="mt-4 text-slate-300 text-center">Build date: {version?.buildDate.split('T')[0]} githash: {version?.commitHash.slice(0,7)}</div>
 {/await}
